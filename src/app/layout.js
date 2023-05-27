b883086f6dac5a44b1b0e1ac4b79df9e1fec5df5
anildalar <dollorinfotech@gmail.com>
2023-05-27 21:05:58 +0530

    new

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,35 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import './globals.css'
-
+import Head from 'next/head'
+import Script from 'next/script';
 
 export const metadata = {
+  charset:'UTF-8',
   title: 'Online coding playground',
   description: 'Online coding playground',
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  }
 }
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        {children}
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-      </body>
-    </html>
+    <>
+      <Head>
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+          <link href="https://craig.global.ssl.fastly.net/rainbow-themes/rainbow.css?2.1.4" rel="stylesheet" type="text/css" media="screen"></link>
+      </Head>
+      <html lang="en">
+        <body>
+          {children}
+          <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous" strategy="afterInteractive" />
+          <Script src="https://craig.global.ssl.fastly.net/js/rainbow/rainbow.min.js?00e3a" strategy="afterInteractive" />
+        </body>
+      </html>
+    </>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,4 @@
-'use client'
 //1. Import area
-
 
 
 //2. Defination area RFC/RCC
@@ -11,42 +9,53 @@ function Home({ data }) {
 
     //2.3 return statement
     return (
-     <div className="container-fluid p-5 pt-2">
-         <p>Data: {console.log(data)}</p>
-        <h1 className="text-center mb-3">Online Code Playground</h1>
-        <div className="d-flex flex-row mb-2">
-          <select className="form-select a_w10 me-2" aria-label="Default select example">
-            <option defaultValue>Select Langauage</option>
-            {/* {
-              langauages.lenght > 0 && langauages.data.map((cv,idx,arr)=>{
-                return <option key={idx} value={1}>{cv}</option>
-              })
-            } */}
-            
-          </select>
-          <select className="form-select a_w10 me-2" aria-label="Default select example">
-            <option defaultValue>Select Version</option>
-            <option value={1}>2</option>
-            <option value={2}>3</option>
-          </select>
-          <button className="btn btn-success a_w5">Run <i className="fa-solid fa-chevron-right" /> </button>
-        </div>
-        <div className="row m-0">
-          <div className="col-6 ps-0 pe-1  a_h600">
-            <div className="shadow-sm bg-white h-100 p-2" contentEditable="true" id="editableContent" suppressContentEditableWarning={true}>1</div>
+      <>
+        <div className="container-fluid p-5 pt-4">
+          <h1 className="text-center mb-4">Online Code Playground</h1>
+          <div className="row m-0 mb-2">
+            <div className="col-12 col-sm-2 p-0 mb-2 me-2">
+              <select className="form-select shadow-sm w-100" aria-label="Default select example">
+                <option>Select Langauage</option>
+                <option defaultValue value="python">Python</option>
+                <option value="java">Java</option>
+                <option value="php">PHP</option>
+                <option value="c">C</option>
+                <option value="cpp">C++</option>
+                <option value="csharp">C#</option>
+                <option value="r">R</option>
+              </select>
+            </div>
+            <div className="col-12 col-sm-2 p-0 mb-2 me-2">
+              <select className="form-select shadow-sm w-100" aria-label="Default select example">
+                <option>Select Version</option>
+                <option value={1}>2</option>
+                <option defaultValue value={2}>3</option>
+              </select>
+            </div>
+            <div className="col-12 col-sm-2 p-0 mb-2">
+              <button className="btn btn-success shadow-sm">
+                Run <i className="fa-solid fa-chevron-right" />
+              </button>
+            </div>
           </div>
-          <div className="col-6 pe-0 ps-1 a_h600 ">
-            <div className=" shadow-sm bg-black text-white h-100 p-2 a_consola">
-              Hello, World!
-              <div className="d-flex justify-content-center align-items-center h-100">
-                <div className="position-absolute loader text-success spinner-border" role="status">
-                  <span className="visually-hidden">Loading...</span>
+          <div className="row m-0">
+            <div className="col-12 bg-white a_c1 col-lg-6 shadow-sm ps-0 pe-1 a_h600 python">
+              <pre className="p-2 h-100 mb-0 shadow-none" contentEditable="true" suppressContentEditableWarning={true}><code data-language="python" className="h-100 shadow-none">def openFile(path):{"\n"}{"  "}file = open(path, "r"){"\n"}{"  "}content = file.read(){"\n"}{"  "}file.close(){"\n"}{"  "}return content{"\n"}{"              "}</code>{"\n"}{"              "}</pre>
+            </div>
+            <div className="position-relative  col-12 col-lg-6 pe-0 ps-1 a_h600">
+              <div className="shadow-sm bg-black text-white h-100 p-2 a_consola">
+                Hello, World!
+                <div className="position-absolute top-0 start-0 d-flex justify-content-center align-items-center h-100 w-100">
+                  <div className="loader text-success spinner-border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </>
+
     )
 }
 
