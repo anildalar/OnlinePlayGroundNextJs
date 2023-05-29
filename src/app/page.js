685e1d601d774a0e416bbcb6bd9cd7fa62ad9a15
anildalar <dollorinfotech@gmail.com>
2023-05-29 18:41:19 +0530

    new

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,24 @@
 //1. Import area
 import A from "@/components/child/A";
-
-
-
+import { URL } from "@/helper/helper";
 
 //2. Defination area RFC/RCC
 async function Home() {
-  const res = await fetch('http://localhost:3000/api/getAllLanguages2',{ cache:'no-store'});
+  const res = await fetch(URL+'/api/getAllLanguages2',{ cache:'no-store'});
   const langs = await res.json();
 
-    //2.3 functions defination
-    
-    console.log(langs);
-    //2.3 return statement
-    return (
-      <>
-        <div className="container-fluid p-4 pt-3">
-          <h1 className="text-center mb-4">Online Code Playground</h1>
-          <A langs={langs} />
-        </div>
-      </>
-    )
+  //2.3 functions defination
+  
+  console.log(langs);
+  //2.3 return statement
+  return (
+    <>
+      <div className="container-fluid p-4 pt-3">
+        <h1 className="text-center mb-4">Online Code Playground</h1>
+        <A langs={langs} />
+      </div>
+    </>
+  )
 }
 
 //3 Export area
